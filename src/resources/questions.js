29468c37e059dc6383f3ce512bd950968c851484
Tora Kessler <tora.kessler@gmail.com>
2018-04-18 08:56:10 +0200

    Added questions

---
+++ src/resources/questions.js
@@ -0,0 +1,162 @@
+[
+  {
+    "question": "What is the capital of New Zealand?",
+    "answer": "Wellington",
+  },
+  {
+    "question": "Christiana is the former name of which European city?",
+    "answer": "Oslo",
+  },
+  {
+    "question": "Dushanbe is the capital of which Central Asian republic?",
+    "answer": "Tajikistan",
+  },
+  {
+    "question": "In which European city would you find the Spanish Riding School?",
+    "answer": "Vienna",
+  },
+  {
+    "question": "Until 1868, which city was the capital of Japan?",
+    "answer": "Kyoto",
+  },
+  {
+    "question": "Which US city hosted the 1904 Summer Olympic Games?",
+    "answer": "St Louis",
+  },
+  {
+    "question": "Which Canadian city is home to the ice hockey team the Maple Leafs?",
+    "answer": "Toronto",
+  },
+  {
+    "question": "What is the most northern capital city in Europe?",
+    "answer": "Reyjkavik",
+  },
+  {
+    "question": "In which city would you find the famous Phoenix Park?",
+    "answer": "Dublin",
+  },
+  {
+    "question": "Which is further east, Portsmouth or Southampton?",
+    "answer": "Portsmouth",
+  },
+  {
+    "question": "Which U.S City is the home of the Mowton Record Company?",
+    "answer": "Detroit",
+  },
+  {
+    "question": "In which city is the Prado art gallery?",
+    "answer": "Madrid",
+  },
+  {
+    "question": "Which artistic Italian city was almost destroyed by flood in 1966?",
+    "answer": "Florence",
+  },
+  {
+    "question": "Which American city was once called New Amsterdam?",
+    "answer": "New York",
+  },
+  {
+    "question": "Which city is dominated by Sugar loaf mountain?",
+    "answer": "Rio de Janeiro",
+  },
+  {
+    "question": "Which British city has an underground railway system which is nicknamed \"The Clockwork Orange\"?",
+    "answer": "Glasgow",
+  },
+  {
+    "question": "In which city is the European court of justice?",
+    "answer": "Luxembourg",
+  },
+  {
+    "question": "In which city is Tianamen Square where over 1,000 demonstraters were killed in 1989?",
+    "answer": "Beijing",
+  },
+  {
+    "question": "What is the capital of Turkey?",
+    "answer": "Ankara",
+  },
+  {
+    "question": "Which Turkish city has the name of a cartoon character?",
+    "answer": "Batman",
+  },
+  {
+    "question": "What is the noisiest city in the world?",
+    "answer": "Hong Kong",
+  },
+  {
+    "question": "What is the capital of the American state Hawaii?",
+    "answer": "Honolulu",
+  },
+  {
+    "question": "What is the capital city of Australia?",
+    "answer": "Canberra",
+  },
+  {
+    "question": "Name the Capital city of Armenia?",
+    "answer": "Yerevan",
+  },
+  {
+    "question": "In which Capital city would you find the Eiffel Tower?",
+    "answer": "Paris",
+  },
+  {
+    "question": "What is the largest city in Missouri, USA?",
+    "answer": "Kansas City",
+  },
+  {
+    "question": "What is the Capital of Greece?",
+    "answer": "Athens",
+  },
+  {
+    "question": "Big Ben can be found in which British Capital city?",
+    "answer": "London",
+  },
+  {
+    "question": "The 'Forbidden City' can be found in which Capital city?",
+    "answer": "Beijing",
+  },
+  {
+    "question": "Which Capital city does the Aare river pass through?",
+    "answer": "Bern",
+  },
+  {
+    "question": "What is the Capital of Germany?",
+    "answer": "Berlin",
+  },
+  {
+    "question": "Which English town was a forerunner of the Parks Movement and the first city in Europe to have a street tram system?",
+    "answer": "Birkenhead",
+  },
+  {
+    "question": "What is the capital of Morocco?",
+    "answer": "Rabat",
+  },
+  {
+    "question": "What is the capital of Egypt?",
+    "answer": "Cairo",
+  },
+  {
+    "question": "What is the most populated city in North America?",
+    "answer": "Mexico City",
+  },
+  {
+    "question": "In which city can you find the Golden Gate Bridge?",
+    "answer": "San Francisco",
+  },
+  {
+    "question": "Where can you find the tallest building, Burj Khalifa?",
+    "answer": "Dubai",
+  },
+  {
+    "question": "What city has the most skyscrapers in the world?",
+    "answer": "Hong Kong",
+  },
+  {
+    "question": "Which city is the largest in the world by population?",
+    "answer": "Tokyo",
+  },
+  {
+    "question": "Where can you find the unfinished church Sagrada Família?",
+    "answer": "Barcelona",
+  }
+]
